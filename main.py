22daed0c078c9efb39e9239bd374d271a62ed3b7
jairodri <rodrig43@gmail.com>
2024-12-07 12:11:16 +0100

    added automated tects

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import os
 from modules.exportfiles import generar_csv_clientes  
+from test.tests import generar_dataframe_prueba_clientes
 
 def main():
     # Simular un DataFrame con datos de ejemplo
-    datos_clientes = {
-        "SIRET": ["12345678901234", "abcdefghij1234"],
-        "Customer ID": ["CUST001", None],
-        "Customer title": ["Mr", "Ms"],
-        "Vehicle registration": ["123ABC", "ABC-123"],
-        "Personal data": [31, None],
-    }
-    df_clientes = pd.DataFrame(datos_clientes)
+    df_prueba = generar_dataframe_prueba_clientes()
+    df_clientes = pd.DataFrame(df_prueba)
 
     # Definir la ruta de salida
     ruta_salida = "data/clients.csv"
